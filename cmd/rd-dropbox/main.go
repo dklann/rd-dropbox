5f565bdb882cbf13a5eb38de8a016d51c826496b
David Klann <dklann@linux.com>
2018-02-09 10:09:01 -0600

    Current working version.

--- cmd/rd-dropbox/main.go
+++ cmd/rd-dropbox/main.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"path"
 
 	_ "github.com/go-sql-driver/mysql"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -16,52 +18,20 @@ type credentials struct {
 	database string
 }
 
-type rowDropbox struct {
+type RowDropbox struct {
 	id      int
 	path    string
 	logPath string
-}
-
-// Snarf all the rows in the DROPBOX table and return a slice of rowDropbox.
-func getDropboxPaths(db *sql.DB, verbose bool) []rowDropbox {
-	var rowCount int
-	var thisRow rowDropbox
-
-	// How many paths are we looking at here?
-	row := db.QueryRow("SELECT count(id) FROM DROPBOXES")
-	err := row.Scan(&rowCount)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if verbose {
-		fmt.Printf("Found %d dropboxes\n", rowCount)
-	}
-
-	paths := make([]rowDropbox, rowCount, rowCount+1)
-
-	rows, err := db.Query("SELECT id,path,log_path FROM DROPBOXES")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for rows.Next() {
-		if err := rows.Scan(&thisRow.id, &thisRow.path, &thisRow.logPath); err != nil {
-			log.Fatal(err)
-		}
-		paths = append(paths, thisRow)
-	}
-
-	return paths
 }
 
 var (
 	verbose = kingpin.Flag("verbose", "Be chatty when running").Short('v').Bool()
 )
 
-func main() {
-	kingpin.CommandLine.HelpFlag.Short('h')
-	kingpin.Version("0.0.1")
-	kingpin.Parse()
+// Snarf all the rows in the DROPBOX table and return a slice of rowDropbox.
+func getDropboxPaths(paths []RowDropbox) []RowDropbox {
+	var rowCount int
+	var thisRow RowDropbox
 
 	db, err := sql.Open("mysql", "rduser:8-XNmVk1WYHYgXEE@tcp(service)/Rivendell")
 	if err != nil {
@@ -79,11 +49,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	paths := getDropboxPaths(db, *verbose)
+	// How many paths are we looking at here?
+	row := db.QueryRow("SELECT count(*) FROM DROPBOXES")
+	err = row.Scan(&rowCount)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *verbose {
+		fmt.Printf("Found %d dropboxes\n", rowCount)
+	}
+
+	rows, err := db.Query("SELECT id,path,log_path FROM DROPBOXES")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&thisRow.id, &thisRow.path, &thisRow.logPath); err != nil {
+			log.Fatal(err)
+		}
+		paths = append(paths, thisRow)
+	}
 
 	if *verbose {
-		for i := range paths {
-			fmt.Printf("%d %s %s\n", paths[i].id, paths[i].path, paths[i].logPath)
+		fmt.Printf("before returning: paths: %v\n", paths)
+	}
+	return paths
+}
+
+func main() {
+	var paths []RowDropbox
+
+	kingpin.CommandLine.HelpFlag.Short('h')
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	paths = getDropboxPaths(paths)
+
+	for i := range paths {
+		fmt.Printf("Dropbox ID: %d, Path:%s, Log Path:%s\n", paths[i].id, paths[i].path, paths[i].logPath)
+
+		if pathInfo, err := os.Stat(path.Dir(paths[i].path)); os.IsNotExist(err) {
+			fmt.Printf("%s does not seem to exist.\n", path.Dir(paths[i].path))
+		} else {
+			if *verbose {
+				fmt.Printf("Dir %v, mode: %o\n", pathInfo.IsDir(), pathInfo.Mode())
+			}
 		}
 	}
 }
